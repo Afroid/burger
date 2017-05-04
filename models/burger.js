ead64cbe32a184094e232a7b90790b018f781595
John Brady <John Brady>
2017-05-03 22:52:19 -0400

    Adding public directory and setup for the View Directory

--- models/burger.js
+++ models/burger.js
@@ -3,20 +3,20 @@ var orm = require("../config/orm.js");
 
 var  = {
   selectAll: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
+    // orm.all("burgers", function(res) {
+    //   cb(res);
+    // });
   },
   // The variables cols and vals are arrays.
   insertOne: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
-      cb(res);
-    });
+    // orm.create("burgers", cols, vals, function(res) {
+    //   cb(res);
+    // });
   },
   updateOne: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
+    // orm.update("burgers", objColVals, condition, function(res) {
+    //   cb(res);
+    // });
   }
 };
 
